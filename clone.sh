20d8c6db9ac36a6f23e9d4b298aee11b5dd9f5ea
송민우 <ssong@songmin-uui-iMac.local>
2021-05-28 17:44:49 +0900

    first commit

---
+++ clone.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+rm -rf ./jpetstore-msa-frontend
+rm -rf ./jpetstore-msa-accounts
+rm -rf ./jpetstore-msa-banners
+rm -rf ./jpetstore-msa-carts
+rm -rf ./jpetstore-msa-categories
+rm -rf ./jpetstore-msa-inventories
+rm -rf ./jpetstore-msa-items
+rm -rf ./jpetstore-msa-products
+rm -rf ./jpetstore-msa-suppliers
+
+git clone https://github.com/nationminu/jpetstore-msa-frontend
+git clone https://github.com/nationminu/jpetstore-msa-accounts
+git clone https://github.com/nationminu/jpetstore-msa-banners
+git clone https://github.com/nationminu/jpetstore-msa-carts
+git clone https://github.com/nationminu/jpetstore-msa-categories
+git clone https://github.com/nationminu/jpetstore-msa-inventories
+git clone https://github.com/nationminu/jpetstore-msa-items
+git clone https://github.com/nationminu/jpetstore-msa-products
+git clone https://github.com/nationminu/jpetstore-msa-suppliers
+
